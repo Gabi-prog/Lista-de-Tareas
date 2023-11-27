d9f65638e5ded594cfc928d9272a7c164adfdcf4
gabiPer <gabriela.r.peralta@gmail.com>
2023-11-27 16:34:46 -0300

    Eliminacion logo, creacion titulo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 
 import './App.css';
-import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
 import Tarea from './Componentes/Tarea';
 import ListaDeTareas from './Componentes/ListaDeTareas';
 
 function App() {
   return (
     <div className='aplicacion-tareas'>
-      <div className='logo-contenedor'>
-        <img  
-        src={ freeCodeCampLogo }  
-        className='logo' 
-        />
+      <div className='logo-contenedor'>       
+        <h1 id='titulo-principal'>Curso React-UTN</h1>
       </div>
       <div  className='tareas-lista-principal'>
         <h1>Mis Tareas</h1>
